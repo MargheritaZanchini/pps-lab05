15c72b26c3fe95239c4e348eee4ef8edafdc3806
Margherita Zanchini <margherita.zanchini@studio.unibo.it>
2026-04-13 11:46:57 +0200

    Exercise 2 done

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -1,11 +1,28 @@
 package it.unibo.pps.polyglot.a05b
 
 import it.unibo.pps.polyglot.a05b.Logics
+import scala.util.Random
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
+  private var firstRide = true
+  private val random = scala.util.Random()
+  private var tickCount = 0
+  private val initial = (random.nextInt(size - 2) + 1, random.nextInt(size - 2) + 1)
+  println(initial)
 
-  override def isOver: Boolean = false
+  override def tick(): Unit = {
+    tickCount = tickCount + 1
+  }
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def isOver: Boolean = initial match {
+    case (xi, yi) => (yi - tickCount) < 0 || yi + tickCount >= size ||
+      xi - tickCount < 0 || xi + tickCount >= size
+  }
+
+  override def hasElement(x: Int, y: Int): Boolean = initial match {
+    case (xi, yi) => x == xi && (y - yi).abs <= tickCount ||
+      y == yi && (x - xi).abs <= tickCount ||
+      (x - y) == (xi - yi) && (x - xi).abs <= tickCount ||
+      (x + y) == (xi + yi) && (x - xi).abs <= tickCount
+  }
